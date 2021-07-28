750edb8b4ad2f6314d5ae55fcf3f62cc3413dc99
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-07-28 20:58:41 +0100

    wip: borrado individual

--- lib/screens/maps_history.dart
+++ lib/screens/maps_history.dart
@@ -15,12 +15,29 @@ class MapsHistoryScreen extends StatelessWidget {
     return ListView.builder(
       physics: BouncingScrollPhysics(),
       itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-        leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-        title: Text(scans[i].value),
-        subtitle: Text(scans[i].id.toString()),
-        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-        onTap: () => print(scans[i].id.toString())
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        direction: DismissDirection.endToStart,
+        onDismissed: (DismissDirection direction) {
+          Provider.of<ScanListProvider>(context, listen: false).deleteScan(scans[i].id!);
+        },
+        background: Container(
+          padding: EdgeInsets.only(right: 20.0),
+          color: Colors.redAccent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.delete, color: Colors.white)
+            ],
+          ),
+        ),
+        child: ListTile(
+          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+          title: Text(scans[i].value),
+          subtitle: Text(scans[i].id.toString()),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print(scans[i].id.toString())
+        ),
       )
     );
   }
