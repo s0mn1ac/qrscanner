4e8a7072d07ee674312415c9fe5c04f8c5efdc94
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-07-29 14:22:59 +0100

    add: instalación y configuración inicial de Google Maps

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -1,10 +1,28 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:qrscanner/providers/db_provider.dart';
 
-class MapsScreen extends StatelessWidget {
+class MapsScreen extends StatefulWidget {
+
+  @override
+  _MapsScreenState createState() => _MapsScreenState();
+}
+
+class _MapsScreenState extends State<MapsScreen> {
+
+  Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
+
+    final CameraPosition initialPoint = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
 
     final Scan scan = ModalRoute.of(context)!.settings.arguments as Scan;
     
@@ -14,9 +32,13 @@ class MapsScreen extends StatelessWidget {
         title: Text('Mapa'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(scan.value),
-      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPoint,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        }
+      )
     );
   }
 }
